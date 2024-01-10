a837283d45b483d7ed156f145a740e21ded65a02
RankillerDY <rankillerdy@gmail.com>
2024-01-10 22:59:01 +0700

    Express Router

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 const express = require('express')
 const morgan = require('morgan')
 const http = require('http')
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = '5000';
 const app = express();
 
 app.use(morgan('dev'));
-
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
-// app.use((req, res, next) => {
-//     console.log(req.headers);
-//     res.statusCode = 200;
-//     res.setHeader('Content-Type', 'text/html');
-//     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+const nationRouter = require('./nationRouter');
 
-// });
+app.use('/nations', nationRouter);
+
 app.get("/", (req, res) => {
     console.log(req.headers);
     res.statusCode = 200
